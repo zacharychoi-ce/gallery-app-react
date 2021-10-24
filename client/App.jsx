e78532e109004ebf5d3cbf02d186456328c930df
zacharychoi-ce <zach7choi@gmail.com>
2021-10-24 23:55:21 +1300

    create and update components, setup App.jsx Routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,21 +1,30 @@
-import React, { useState, useEffect } from 'react'
-// import { getGreeting } from '../apiClient'
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import Header from './components/Header'
+import About from './components/About'
+import Contact from './components/Contact'
+import CreatePost from './components/CreatePost'
+import PostDetails from './components/PostDetails'
+import CreateMsg from './components/Messages/CreateMsg'
+import DeleteMsg from './components/Messages/DeleteMsg'
 
 const App = () => {
-  // const [greeting, setGreeting] = useState('')
-  // const [count, setCount] = useState(0)
-
-  // useEffect(() => {
-  //   getGreeting()
-  //     .then((greeting) => {
-  //       console.log(greeting)
-  //       setGreeting(greeting)
-  //     })
-  // }, [count])
-
   return (
     <>
-
+      <div>
+        <Header />
+        <main>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/createpost' component={CreatePost} />
+          <Route path='/post/:id' component={PostDetails} />
+          <Route path='/createmsg' component={CreateMsg} />
+          <Route path='/deletemsg' component={DeleteMsg} />
+        </main>
+      </div>
     </>
   )
 }
